96c245e2384c8ebccaa1ebb27fbb86faa331e8d7
vladislav77777 <vladoss7700@mail.ru>
2023-05-18 11:07:37 +0300

    v 2.0

--- src/tictactoe/Button.java
+++ src/tictactoe/Button.java
@@ -2,12 +2,47 @@ package tictactoe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 
 public class Button extends JButton {
+    private static final int ARC_WIDTH = 10; // Adjust the arc width as desired
+    private static final int ARC_HEIGHT = 10; // Adjust the arc height as desired
+
     public Button(String text) {
         super(text);
-        this.setBorder(new RoundedBorder(8));
+        this.setContentAreaFilled(false);
+        this.setFocusPainted(false);
+        this.setBorderPainted(false);
         this.setFont(new Font("Arial", Font.BOLD, 40));
+    }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        if (getModel().isPressed()) {
+            g2.setColor(getBackground().darker());
+        } else if (getModel().isRollover()) {
+            g2.setColor(getBackground().brighter());
+        } else {
+            g2.setColor(getBackground());
+        }
+
+        // Create a rounded rectangle shape
+        Shape shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, ARC_WIDTH, ARC_HEIGHT);
+        g2.fill(shape);
+
+        g2.setColor(getForeground());
+
+        FontMetrics fm = g2.getFontMetrics();
+        int x = (getWidth() - fm.stringWidth(getText())) / 2;
+        int y = (getHeight() - fm.getHeight()) / 2 + fm.getAscent();
+
+        g2.drawString(getText(), x, y);
+
+        g2.dispose();
     }
 }
